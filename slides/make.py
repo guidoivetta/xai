e6bfc33024c2b0fdd3249eafa9291f87aa4890b7
Paula Pacheco <paulapacheco@mi.unc.edu.ar>
2026-05-11 11:58:52 -0300

    Update make.py script from upstream to sync with latest changes

--- slides/make.py
+++ slides/make.py
@@ -48,11 +48,21 @@ SYNTAX_HIGHLIGHT = [
     "haddock",
 ]
 
-PANDOC_CMD_TEMPLATE = sh.Command("pandoc").bake(
+pandoc = sh.Command("pandoc")
+
+
+def _pandoc_version():
+    version_output = pandoc(version=True)
+    version_line = version_output.splitlines()[0]
+    version_string = version_line.split()[-1]
+    version_tuple = tuple(version_string.split("."))
+    return version_tuple
+
+
+PANDOC_CMD_TEMPLATE = pandoc.bake(
     t="beamer",
     d="../defaults.yaml",
-    # filter="pandoc-citeproc",
-    citeproc=True,
+    filter="pandoc-citeproc",
     highlight_style="../sh_style.theme",
     verbose=True,
 )
